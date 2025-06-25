0275645fbde0047fa49cdf2b75e3f4245a529256
jay-7314 <hjh268100@gmail.com>
2025-06-25 22:03:50 +0900

    [Asset 추가] 총/수류탄 Asset추가
    총/수류탄 Asset추가 했습니다. 현재 IK를 적용시키고 있는데 약간 오류가 있어 명일에 진행하겠습니다.

---
+++ Assets/Scripts/IKScripts.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+//총과 수류탄을 더 자연스럽게 잡기위해 IKscripts 작성
+public class IKScripts : MonoBehaviour
+{
+    Animator anim;
+    public Transform leftHand;
+    public Transform rightHand;
+
+    private void OnAnimatorIK(int layerIndex)
+    {
+        anim.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1);
+        anim.SetIKRotationWeight(AvatarIKGoal.LeftHand, 1);
+        anim.SetIKPosition(AvatarIKGoal.LeftHand, leftHand.position);
+        anim.SetIKRotation(AvatarIKGoal.LeftHand, leftHand.rotation);
+
+        anim.SetIKPositionWeight(AvatarIKGoal.RightHand, 1);
+        anim.SetIKRotationWeight(AvatarIKGoal.RightHand, 1);
+        anim.SetIKPosition(AvatarIKGoal.RightHand, rightHand.position);
+        anim.SetIKRotation(AvatarIKGoal.RightHand, rightHand.rotation);
+
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
