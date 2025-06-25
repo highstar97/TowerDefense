399532360f670de25a111eab7a73b76d7b8e72ce
jay-7314 <hjh268100@gmail.com>
2025-06-25 18:44:30 +0900

    throw maximo다운 받음
    maximo로 다운 받음

--- Assets/ARAScripts/CamRotate.cs
+++ Assets/ARAScripts/CamRotate.cs
@@ -7,6 +7,8 @@ using UnityEngine;
 // 필요속성 : 현재각도, 마우스감도
 public class CamRotate : MonoBehaviour
 {
+    //public Transform target; //케릭터가 카메라를 따라감
+    //public Vector3 offset;
     // 현재각도
     Vector3 angle;
     // 마우스감도
@@ -32,6 +34,7 @@ public class CamRotate : MonoBehaviour
         // 이동 공식에 대입하여 각 속성별로 회전 값을 누적 시킨다.
         angle.x += x * sensitivity * Time.deltaTime;
         angle.y += y * sensitivity * Time.deltaTime;
+        angle.y = Mathf.Clamp(angle.y, -40f, 70f);
 
         angle.y = Mathf.Clamp(angle.y, -90, 90);
         // 3. 회전 시키고 싶다.
