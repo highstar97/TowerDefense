38b87af626cffe1e04590c8a835d287025e8149e
jay-7314 <hjh268100@gmail.com>
2025-06-27 14:13:44 +0900

    [EndingScene완성]
    BadEnding, HappyEnding씬 완성했습니다.

---
+++ Assets/Scripts/Ending_Win.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Ending_Win : MonoBehaviour
+{
+    public Button retryBtn; //게임 재시작
+    public Button gameoverBtn; //게임종료
+
+    //게임 재시작
+    public void retry()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //게임종료
+    public void GameOver()
+    {
+        Application.Quit();
+    }
+
+    public void Playtime()
+    {
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
